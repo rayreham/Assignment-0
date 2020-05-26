a4b42212f352dfcd5096767c15d6c5c1c8184281
rayreham <rayreham@yahoo.com>
2020-05-26 18:51:35 -0400

    Answer

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,6 +1,25 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  var arr = []; //declare empty array
+ 
+  for(var i = start; i <= end; i++){
+    //checks conditionals and pushes appropriate values
+    if (i % 3 == 0 && i % 5 == 0) {
+      arr.push("FizzBuzz");
+    } 
+    else if (i % 5 == 0) {
+      arr.push("Buzz");
+    } 
+    else if (i % 3 == 0) {
+      arr.push("Fizz");
+    } 
+    else {
+      arr.push(i);
+    }
+  }
+  //console.log(arr);
+  return arr;
 }
 
 // Do not edit this line;
 module.exports = fizzBuzz;
+//fizzBuzz(1, 15);
