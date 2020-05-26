ce08862ad34f4d30825bbf3b25e1baec9940b3ba
rayreham <rayreham@yahoo.com>
2020-05-26 17:26:50 -0400

    Answer

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,16 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+  //variable count serves as tracker
+  let count = 0;
+
+  //iterates through array and increments count when conditional is true
+  for(var i = 0; i < arr.length; i++){
+    //conditional uses typeof operater to check data type of element
+    if(typeof arr[i] === 'boolean'|| typeof arr[i] === 'string' ){
+      count++;
+    }
+  }
+  //console.log(count);
+  return count;
 }
 
 // Do not edit this line;
