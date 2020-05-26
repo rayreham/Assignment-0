186798912c0039ccd1daa8b628a5c9befcea5740
rayreham <rayreham@yahoo.com>
2020-05-26 18:08:07 -0400

    Answer

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,6 +1,17 @@
 function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
+  //despite signature, description of function is to count even numbers
+  let count = 0;
+
+  for(var i = 0; i < nums.length; i++){
+    //usues modulus to find remainder
+    if(nums[i] % 2 == 0){
+      count++;
+    }
+  }
+  //console.log(count);
+  return count;
 }
 
 // Do not edit this line;
 module.exports = sumOfAllEvenNumbers;
+//sumOfAllEvenNumbers([2,4,6,7,8,9,10])
