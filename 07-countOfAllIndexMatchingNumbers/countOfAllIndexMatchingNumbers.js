59e13398cea4505534366ca2cdb312b1787f55ab
rayreham <rayreham@yahoo.com>
2020-05-26 17:55:40 -0400

    Answer

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,6 +1,15 @@
 function countOfAllIndexMatchingNumbers(nums) {
-  // Insert code here;
+  //tracker variable
+  let count = 0;
+  for(var i = 0; i < nums.length; i++){
+    if(nums[i] == i){
+      count++;
+    }
+  }
+  //console.log(count);
+  return count;
 }
 
 // Do not edit this line;
 module.exports = countOfAllIndexMatchingNumbers;
+//countOfAllIndexMatchingNumbers([0,2,3,4,4,5,77]);
