39f6e789fa6e384de4e9e166df21b69ae3833b78
rayreham <rayreham@yahoo.com>
2020-05-26 19:45:36 -0400

    Answer

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,11 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  let product = 1; //intilize to 1 
+
+  for(var i = 0; i < args.length; i++){
+    product *= args[i]; //multiply args
+  }
+  //console.log(product);
+  return product;
 }
 
 // Do not edit this line;
