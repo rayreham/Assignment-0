c5faaf85f42cc164814da5a7eae7c6445f32def7
rayreham <rayreham@yahoo.com>
2020-05-26 18:58:46 -0400

    Answer

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,17 @@
 function isPalindrome(word) {
-  // Insert code here;
+  let z = word.length-1;//counter starting from end of array
+  let palindrome = false;
+
+  for(var i = 0; i < word.length; i++){
+    if(word[i] == word[z]){//checks if values are equal
+      palindrome = true;
+    }
+    z++;
+  }
+  //console.log(palindrome);
+  return palindrome;
 }
 
 // Do not edit this line;
 module.exports = isPalindrome;
+//isPalindrome("racecar")
