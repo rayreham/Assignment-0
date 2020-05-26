cde114f91ccf100469b97e0cebb32342acbbedb4
rayreham <rayreham@yahoo.com>
2020-05-26 18:27:02 -0400

    Answer

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,6 +1,15 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+  var count = 0;
+  var i = nums.indexOf(start);
+
+  //counts elements from start to end
+  for(i; i <= nums.indexOf(end); i++){
+      count++;
+  }
+  //console.log(count);
+  return count;
 }
 
 // Do not edit this line;
 module.exports = sumOfNumsWithinARange;
+//sumOfNumsWithinARange([2,4,5,6,23], 4, 6)
