6aca50f74346dcfebab8c22fa7b19cc67eab9759
rayreham <rayreham@yahoo.com>
2020-05-26 17:46:01 -0400

    Answer

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,6 +1,16 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
-  // Insert code here;
+  //traker variable
+  let count = 0;
+  //iterates through array and increments counter when applicable
+  for(var i = 0; i < nums.length; i++){
+    if(nums[i] < target){
+      count++;
+    }
+  }
+  //console.log(count);
+  return count;
 }
 
 // Do not edit this line;
 module.exports = countOfAllNumbersSmallerThanTarget;
+//countOfAllNumbersSmallerThanTarget([1,5,3,2,20,18], 18);
