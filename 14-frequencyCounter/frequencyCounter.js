0343f7ea7f0a4dda1e4eaa21eed1c1a4665f767b
rayreham <rayreham@yahoo.com>
2020-05-26 19:31:09 -0400

    Answer

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,6 +1,22 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  let mapper = {};//decalre empty map
+
+  for (var i = 0; i < word.length; i++) {
+    var val = word.charAt(i);
+
+    if (word[i] in mapper) {    //check if charcater is already mapped
+      mapper[val]++;  //incrments if true
+    }
+    else {            
+      mapper[val] = 1;  //adds to map if not
+    }
+
+  }
+  //console.log(mapper);
+  return mapper;
+    
 }
 
 // Do not edit this line;
 module.exports = frequencyCounter;
+//frequencyCounter("cheese");
