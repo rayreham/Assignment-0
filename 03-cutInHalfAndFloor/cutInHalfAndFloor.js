20f6bdd8fe42873896fafc295a8b49db7dfd64d4
rayreham <rayreham@yahoo.com>
2020-05-26 16:37:06 -0400

    Answer

--- 03-cutInHalfAndFloor/cutInHalfAndFloor.js
+++ 03-cutInHalfAndFloor/cutInHalfAndFloor.js
@@ -1,5 +1,8 @@
 function cutInHalfAndFloor(num) {
-  // Insert code here;
+  //divides by two then rounds down to nearest integer using floor method
+  num = Math.floor(num/2);
+  //console.log(num);
+  return num;
 }
 
 // Do not edit this line;
