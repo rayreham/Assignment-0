a52d8fb4e8528f2fc74ff9237d2bc59a303e0f8c
rayreham <rayreham@yahoo.com>
2020-05-26 18:32:30 -0400

    Answer

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,6 +1,24 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  //intial values of high and low are first elements
+  let low = nums[0];
+  let high = nums[0];
+
+  //iterator that checks truth values of conditions
+  for(var i = 1; i < nums.length; i++){
+    if(nums[i] < low){
+      low = nums[i];
+    }
+    if(nums[i] > high){
+      high = nums[i];
+    }
+  }
+
+  let sum = low + high;
+
+  //console.log(sum);
+  return sum;
 }
 
 // Do not edit this line;
 module.exports = sumOfMinimumAndMaximum;
+//sumOfMinimumAndMaximum([2,4,5,6,23])
