3ed3e050a63fa13442847f04de24bcb5cd43b51f
rayreham <rayreham@yahoo.com>
2020-05-26 16:23:17 -0400

    Answer

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,17 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  //splits the elememts at every space value
+  var titleArray = title.split(" "); 
+  
+  //for loop iterates through the array by element
+  for(var i = 0; i < titleArray.length; i++){
+    //makes first letter uppercase then appends remaining string
+    titleArray[i] = titleArray[i].charAt(0).toUpperCase() + titleArray[i].substr(1);
+    //console.log(titleArray[i]);
+  }
+  //joins elemnts by space and returns newly capatilized title
+  return titleArray.join(" ");
 }
 
 // Do not edit this line;
 module.exports = titleCaseEdit;
+//console.log(titleCaseEdit("Henry ford Jeffery")); test case
