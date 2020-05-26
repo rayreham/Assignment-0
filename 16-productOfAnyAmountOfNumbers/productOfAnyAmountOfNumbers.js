9a953248bf7a6e0702e8c3cb6291a76eb3113a67
rayreham <rayreham@yahoo.com>
2020-05-26 19:51:01 -0400

    Answer

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,6 +1,12 @@
 function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
-}
-
-// Do not edit this line;
-module.exports = productOfAnyAmountOfNumbers;
+    let product = 1; //intilize to 1 
+  
+    for(var i = 0; i < args.length; i++){
+      product *= args[i]; //multiply args
+    }
+    //console.log(product);
+    return product;
+  }
+  
+  // Do not edit this line;
+  module.exports = setUnionOfAnyAmountOfSets;
