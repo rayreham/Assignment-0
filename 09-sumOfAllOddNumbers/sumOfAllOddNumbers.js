78aa9aca76b332da968194b0766e303efcda033c
rayreham <rayreham@yahoo.com>
2020-05-26 18:10:44 -0400

    Answer

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,6 +1,17 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+  //despite signature, description of function is to count odd numbers
+  let count = 0;
+
+  for(var i = 0; i < nums.length; i++){
+    //usues modulus to find remainder
+    if(nums[i] % 2 != 0){
+      count++;
+    }
+  }
+  //console.log(count);
+  return count;
 }
 
 // Do not edit this line;
 module.exports = sumOfAllOddNumbers;
+//sumOfAllOddNumbers([0,1,2,3,4,5,6,7])
