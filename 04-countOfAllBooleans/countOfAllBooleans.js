fe8a753bd40ab4598493714dfd544484cfc86b4b
rayreham <rayreham@yahoo.com>
2020-05-26 16:45:06 -0400

    Answer

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,15 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  //variable count serves as tracker
+  let count = 0;
+
+  //iterates through array and increments count when conditional is true
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i] == true || arr[i] == false){
+      count++;
+    }
+  }
+  //console.log(count);
+  return count;
 }
 
 // Do not edit this line;
