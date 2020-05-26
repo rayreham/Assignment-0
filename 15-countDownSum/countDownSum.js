fce5fec332a79f190871cdce74c07d886a5bb064
rayreham <rayreham@yahoo.com>
2020-05-26 19:38:53 -0400

    Answer

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,9 +1,13 @@
 class MySolution {
   countDownSum(num) {
-    // Insert code here;
+    if(num == 1){ //checks if num is at 0 to terminate
+      return 1;
+    }
+    return num + this.countDownSum(num-1); //recursively calls method to add numbers
   }
 }
 
 // Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
+
