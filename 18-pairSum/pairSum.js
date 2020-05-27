cf4dc4d14ca5e3d6bf207ac1f47bc762722ae897
rayreham <rayreham@yahoo.com>
2020-05-26 20:00:38 -0400

    Answer

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,19 @@
 function pairSum(nums, target) {
-  // Insert code here;
+    let val = false
+    //exception that will be thrown if array is 1 or lower
+    if (nums.length <= 1){
+      throw('Error: Array is 1 or lower');
+    }
+
+    for (var i = 0; i < nums.length - 1; i++) {
+      //iterate through rest of array to compare sum to target
+      for (var j = 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] == target) {
+          val = true;
+        }
+      }
+    }
+    return val;
 }
 
 // Do not edit this line;
