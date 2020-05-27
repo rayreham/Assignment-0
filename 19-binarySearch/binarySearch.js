0cef6e9c00f6e88b48206bc11cc414c2f62837ac
rayreham <rayreham@yahoo.com>
2020-05-26 20:09:45 -0400

    Answer

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -4,7 +4,20 @@ class MySolution {
   }
 
   binarySearch(nums, target) {
-    // Insert code here;
+    let found = false;
+    let middle = Math.ceil(nums.length / 2)-1; //stores middle element
+    let left = nums.slice(0, middle + 1); //stores leftmost
+    let right = nums.slice(middle + 1, nums.length); //stores rightmost
+
+    if (nums[middle] === target)
+      return true;
+    if(1 < nums.length ) {
+      if (nums[middle] > target)
+        found = this.binarySearch(left, target); //recursively calls method to slice again on left side
+      else
+        found = this.binarySearch(right, target); //recursively calls method to slice again on right side
+    }
+    return found; mid
   }
 }
 
